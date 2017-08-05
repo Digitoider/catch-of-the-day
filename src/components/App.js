835707332bd50f2d478e0f67b3a1fc44571504c9
Alexander <digitoider@gmail.com>
2017-08-05 21:45:28 +0300

    Added new react component that is called as App

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header/>
+          <Order/>
+          <Inventory/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
