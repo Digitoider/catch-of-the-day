0c7fefe016e3dccbd4c394972cb1c078083f0fa3
Alexander <digitoider@gmail.com>
2017-08-05 21:33:27 +0300

    Added StorePicker react component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 // let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import './css/styles.css';
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker/>, document.querySelector('#main'));
