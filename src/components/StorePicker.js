3f43d9393e63d3b04392637a18b762b60dec5372
Alexander <digitoider@gmail.com>
2017-08-06 10:51:48 +0300

    I've learned how to reference data from an input and how to run your own functions in the component you are creating

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+  goToStore(event){
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={ (e) => this.goToStore(e) }>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name"/>
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={ (input) => { this.storeInput = input}} />
         <button type="submit">Visit store</button>
       </form>
     );
