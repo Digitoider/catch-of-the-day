0b80bec8041492ac26d5d1722b69ea75e0b62f6b
Alexander <digitoider@gmail.com>
2017-08-07 17:09:30 +0300

    Created a new component AddFishForm. Added functionality to load sample data. Learned about state in React

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,35 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 
+import sampleFishes from '../sample-fishes';
+
 class App extends React.Component {
+
+  constructor(){
+    super();
+
+    this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
+
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish){
+    var fishes = {...this.state.fishes};
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    this.setState( {fishes} );
+  }
+
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +39,7 @@ class App extends React.Component {
           <Header tagline="Fresh sea food market"/>
         </div>
         <Order/>
-        <Inventory/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     );
   }
