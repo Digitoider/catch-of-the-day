cd592d2caf38e6acbbaf7feefd7b5e12a55dfbbf
Alexander <digitoider@gmail.com>
2017-08-10 20:56:42 +0300

    Synced with firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyAPXi6YaH5-WTohTa-BKQX-sQfC1gsBToA",
+  authDomain: "catch-of-the-day-cbf4e.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-cbf4e.firebaseio.com",
+});
+
+export default base; 
